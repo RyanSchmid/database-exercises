b2b1705e7e089ff26239e1a801ded3bb487564f5
ryanschmid <schmid.ryan.c@gmail.com>
2018-10-04 14:10:04 -0500

    MySQL clauses exercises completed

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,10 +1,18 @@
-SELECT DISTINCT title FROM titles;
+USE employees;
 
 -- List the first 10 distinct last name sorted in descending order.
+
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
 
 -- Find your query for employees born on Christmas and hired in the 90s
 --  from order_by_exercises.sql. Update it to find just the first 5 employees.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
+
 
 
 -- Try to think of your results as batches, sets, or pages.
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,27 +1,18 @@
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name = 'Irena' OR
+      first_name = 'Vidya' OR
+      first_name = 'Maya'
+ORDER BY first_name DESC, last_name DESC;
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
 
--- Modify your first query to order by first name.
--- The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
-
-
--- Update the query to order by first name and then last name.
--- The first result should now be Irena Acton and the last should be Vidya Zweizig.
-
-
--- Change the order by clause so that you order by last name before first name.
--- Your first result should still be Irena Acton but now the last result should be Maya Zyda.
-
-
--- Update your queries for employees with 'E' in their last name to sort the results by their employee number.
--- Your results should not change!
-
-
--- Now reverse the sort order for both queries.
-
-
--- Change the query for employees hired in the 90s and born on Christmas
--- such that the first result is the oldest employee who was hired last.
--- It should be Khun Bernini.
-
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date ASC, hire_date DESC;
 
 
