82f4dff6c6cd8a024a9644c1bcc7b11d2becc26b
ryanschmid <schmid.ryan.c@gmail.com>
2018-10-08 11:52:11 -0500

    MySQL subqueries exercise complete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,22 @@
 use employees;
+
+select * from employees e
+where e.hire_date IN (
+select hire_date from employees where emp_no = 101010
+);
+
+select title from titles t where t.emp_no IN (
+select emp_no from employees where first_name = 'Aamod');
+
+
+select e.first_name, e.last_name from employees e where
+e.gender = 'F'
+AND emp_no IN (
+select dm.emp_no from dept_manager dm where to_date > now()
+);
+
+
+
+
+
+
